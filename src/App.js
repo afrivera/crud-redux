2d7bbe182e2c4f318c7a0c14bfe437b29ad1a312
arivera <andresrfelip@hotmail.com>
2022-03-08 22:37:39 -0500

    configuración react-router-dom creando rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Header from "./components/Header";
 
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/" element={<Productos />} />
+          <Route path="/productos/nuevo" element={<NuevoProducto />} />
+          <Route path="/productos/editar/:id" element={<EditarProducto />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
